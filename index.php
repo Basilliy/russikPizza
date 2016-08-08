4d6ad8f59b998b9f5f9aa65bf051f60492bf1112
Basilliy <z.kon2009@gmail.com>
2016-08-08 16:16:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,12 +13,50 @@ $reid = $fb->entry[0]->messaging[0]->recipient->id;
 $message = $fb->entry[0]->messaging[0]->message->text;
 $token = "EAAUZC7GZBxEEoBAOZCZCaTq85jvjA7wIZCvZAyK8NVTMJZAt1yoPZBqhHpavhomi0JJOgDaD6F4z3AVy62vXxX9ToZBZCY0oYo0xTMHBsQlUifR0qF4y3IKSb7CVDp7hQtBoDVWlt3t47so9aMZC0is2QrpKoO2ZBv9puX0rMRzhsTQnoAZDZD";
 $fp = json_decode(file_get_contents('user.json'), true);
-$data = array(
+
+
+$buttonMenu = array(
+        "content_type" => "text",
+        "title" => "Pizza Menu",
+        "payload" => "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_buttonMenu"
+ );
+$buttonSite = array(
+        "content_type" => "text",
+        "title" => "Visit our site",
+        "payload" => "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_buttonSite"
+ );
+ 
+ $buttonLocation = array(
+        "content_type" => "text",
+        "title" => "Our location",
+        "payload" => "DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_buttonLocation"
+ );
+ 
+$keyboardSet =[$buttonMenu,$buttonSite,$buttonLocation];
+
+
+switch ($message) {
+        case 'Generate':
+                                                                                                                                                        look here   |
+           $data = array(
            'recipient' => array('id' => "$id" ),
-           'message' => array("text" => "hi",
-           
+           'message' => array("text" => "Welcome",
+           "quick_replies" => json_encode($keyboardSet)
             )
            );
+        break;
+                                                                                      Look here   |
+          default:
+          $data = array(
+           'recipient' => array('id' => "$id" ),
+           'message' => array("text" => "Welcome",
+           "quick_replies" => json_encode($keyboardSet)
+            )
+           );
+}           
+           
+           
+           
  $options = array(
           'http' => array(
              'method' => 'POST',
