6eacd38e8eb6894fd4a6408a361191d232bafe12
Basilliy <z.kon2009@gmail.com>
2016-08-09 02:12:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,14 @@ $message = $fb->entry[0]->messaging[0]->message->text;
 $token = "EAAUZC7GZBxEEoBAOZCZCaTq85jvjA7wIZCvZAyK8NVTMJZAt1yoPZBqhHpavhomi0JJOgDaD6F4z3AVy62vXxX9ToZBZCY0oYo0xTMHBsQlUifR0qF4y3IKSb7CVDp7hQtBoDVWlt3t47so9aMZC0is2QrpKoO2ZBv9puX0rMRzhsTQnoAZDZD";
 $fp = json_decode(file_get_contents('user.json'), true);
 
+$host = 'upperl.mysql.ukraine.com.ua'; // адрес сервера 
+$database = 'upperl_vadik'; // имя базы данных
+$user = 'upperl_vadik'; // имя пользователя
+$password = '2shmpzez'; // пароль
+$link = mysqli_connect($host, $user, $password,$database)
+    or die('Не удалось соединиться: ' . mysql_error());
+//echo 'Соединение успешно установлено';
+
 
 $buttonMenu = array(
         "content_type" => "text",
@@ -72,7 +80,29 @@ $Map = array(
 
 
 switch ($message) {
-        case 'Generate':                                                                                                                                              
+        case 'Menu':
+           $link =$this->connectToDB();
+           $query = 'SELECT * FROM menu';
+           $result = $link->query($query) or die('Запрос не удался: ' . mysql_error());
+           //print_r($result);
+           $rows = $result->fetch_assoc();
+           //print_r($rows['ButtonsName']);
+           //$mystring = 'Generate Insult,Language,Homepage';
+           $findme   = ',';
+           $button = explode($findme, $rows['pizzaType']);
+           
+           for($i = 0; $i < count($button); $i ++){
+            
+            ${$button[$i]}=  = array(
+            "content_type" => "text",
+            "title" => "$button[$i]",
+            "payload" => "$button[$i]"
+            );
+          $keyboardSet[$i] = $button[$i];
+           }
+
+           
+           
            $data = array(
            'recipient' => array('id' => "$id" ),
            'message' => array("text" => "Welcome",
