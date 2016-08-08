200acb725dff7a72611d9ecec39ed84e1786125d
Basilliy <z.kon2009@gmail.com>
2016-08-08 15:34:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,7 @@
  * Date: 08.08.2016
  * Time: 15:27
  */
-echo'russik say hi';
+$verify_token = "hi"; // Verify token 
+if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) { 
+echo $_REQUEST['hub_challenge']; 
+}
