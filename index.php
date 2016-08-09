6cf45d611b79b090dea6df5cc5358527a645bc67
Basilliy <z.kon2009@gmail.com>
2016-08-09 12:19:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -101,7 +101,7 @@ switch ($message) {
            // $keyboardMenu[$i] = $button[$i];
            // }
             
-           file_put_contents("errors.txt",$button);
+           file_put_contents("errors.txt",$rows);
            $data = array(
            'recipient' => array('id' => "$id" ),
            'message' => array("text" => "Pizza type",
