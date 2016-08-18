56c1081246170ec6869c7e2cd2052d6f51c3a334
Basilliy <z.kon2009@gmail.com>
2016-08-18 10:52:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -111,11 +111,11 @@ switch ($message) {
             $newId = (int) $rowas[$i];
             if($newId == $id){
              $flag = "true";
-              file_put_contents("errors.txt",$newId);
+              
             }
            
            }
-           
+           file_put_contents("errors.txt",$newId);
           // file_put_contents("errors.txt", $rowas);
           // if($flag == true){
                  if (!($stmt = $link->prepare("INSERT INTO russik(user_id) VALUES (?)"))) {
