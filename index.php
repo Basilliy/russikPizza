0cf0dbadcdf69c1322cb2d41722d9f5c2101a53d
Basilliy <z.kon2009@gmail.com>
2016-08-18 10:59:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -108,7 +108,7 @@ switch ($message) {
            //$mystring = 'Generate Insult,Language,Homepage';
            $flag = "false";
            for($i =  0; $i < count($rowas); $i ++){
-            $newId = (int)$rowas[$i];
+            $newId = intval($rowas[$i], 'integer');
             if($newId == $id){
              $flag = "true";
               
