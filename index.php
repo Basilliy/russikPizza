99f03ca914d0190e5a5cb479177acf11b34d0624
Basilliy <z.kon2009@gmail.com>
2016-08-18 13:38:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -209,7 +209,18 @@ $coun = $count->fetch_assoc();
            )
            );
            file_put_contents("errors.txt","Size");
-             break;   
+           UPDATE movies SET filmName = ?
+           if (!($stmt = $link->prepare("UPDATE russik SET pizzaType = ? WHERE user_id = ?))) {
+               echo "Не удалось подготовить запрос: (" . $mysqli->errno . ") " . $mysqli->error;
+                }
+                if (!$stmt->bind_param("si",$message,$id)) {
+                 echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+                }
+                if (!$stmt->execute()) {
+                echo "Не удалось выполнить запрос: (" . $stmt->errno . ") " . $stmt->error;
+                }
+           
+            break;   
              
              case 'Size':                                                                                                                                              
              $findme   = ',';
