90907daa62945ef4b91314fde1f87f0ceb6ac758
Basilliy <z.kon2009@gmail.com>
2016-08-25 13:31:16 +0300

    Update СhangeOrder.php

--- СhangeOrder.php
+++ СhangeOrder.php
@@ -1,6 +1,14 @@
 <?php 
 class СhangeOrder{
   
+  $dbHost='upperl.mysql.ukraine.com.ua';// чаще всего это так, но иногда требуется прописать ip адрес базы данных
+  $dbName='upperl_vadik';// название вашей базы
+  $dbUser='upperl_vadik';// пользователь базы данных
+  $dbPass='2shmpzez';// пароль пользователя
+  $link = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+  $link->set_charset("utf8");
+  
+  
   function printHi(){
     $hi = "russik say hi";
     return $hi;
@@ -8,23 +16,71 @@ class СhangeOrder{
   }
   
   
-  function ChangePizzaType(){
-    
+  function ChangePizzaType($message,$user_id){
+     if (!($stmt = $link->prepare("UPDATE russik SET pizzaType = ? WHERE user_id = ?"))) {
+               echo "Не удалось подготовить запрос: (" . $mysqli->errno . ") " . $mysqli->error;
+                }
+                if (!$stmt->bind_param("si",$message,$user_id)) {
+                 echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+                }
+                if (!$stmt->execute()) {
+                echo "Не удалось выполнить запрос: (" . $stmt->errno . ") " . $stmt->error;
+                }
   }
-  function ChangePizzaSize(){
-    
+  function ChangePizzaSize($message,$user_id){
+    if (!($stmt = $link->prepare("UPDATE russik SET pizzaSize = ? WHERE user_id = ?"))) {
+               echo "Не удалось подготовить запрос: (" . $mysqli->errno . ") " . $mysqli->error;
+                }
+                if (!$stmt->bind_param("si",$message,$user_id)) {
+                 echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+                }
+                if (!$stmt->execute()) {
+                echo "Не удалось выполнить запрос: (" . $stmt->errno . ") " . $stmt->error;
+                }
   }
-  function ChangePizzaQuantity(){
-    
+  function ChangePizzaQuantity($message,$user_id){
+    if (!($stmt = $link->prepare("UPDATE russik SET pizzaQuantity = ? WHERE user_id = ?"))) {
+               echo "Не удалось подготовить запрос: (" . $mysqli->errno . ") " . $mysqli->error;
+                }
+                if (!$stmt->bind_param("si",$message,$user_id)) {
+                 echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+                }
+                if (!$stmt->execute()) {
+                echo "Не удалось выполнить запрос: (" . $stmt->errno . ") " . $stmt->error;
+                }
   }
-  function ChangePhoneNumber(){
-    
+  function ChangePhoneNumber($message,$user_id){
+    if (!($stmt = $link->prepare("UPDATE russik SET phoneNumber = ? WHERE user_id = ?"))) {
+               echo "Не удалось подготовить запрос: (" . $mysqli->errno . ") " . $mysqli->error;
+                }
+                if (!$stmt->bind_param("si",$message,$user_id)) {
+                 echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+                }
+                if (!$stmt->execute()) {
+                echo "Не удалось выполнить запрос: (" . $stmt->errno . ") " . $stmt->error;
+                }
   }
-  function ChangeAdress(){
-    
+  function ChangeAdress($message,$user_id){
+    if (!($stmt = $link->prepare("UPDATE russik SET Adress = ? WHERE user_id = ?"))) {
+               echo "Не удалось подготовить запрос: (" . $mysqli->errno . ") " . $mysqli->error;
+                }
+                if (!$stmt->bind_param("si",$message,$user_id)) {
+                 echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+                }
+                if (!$stmt->execute()) {
+                echo "Не удалось выполнить запрос: (" . $stmt->errno . ") " . $stmt->error;
+                }
   }
-  function ChangePizzaSouce(){
-    
+  function ChangePizzaSouce($message,$user_id){
+    if (!($stmt = $link->prepare("UPDATE russik SET pizzaSouce = ? WHERE user_id = ?"))) {
+               echo "Не удалось подготовить запрос: (" . $mysqli->errno . ") " . $mysqli->error;
+                }
+                if (!$stmt->bind_param("si",$message,$user_id)) {
+                 echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+                }
+                if (!$stmt->execute()) {
+                echo "Не удалось выполнить запрос: (" . $stmt->errno . ") " . $stmt->error;
+                }
   }
 }
 
