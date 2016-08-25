348ab6f6225f2231a04b9b65d2611deb43b5d98d
Basilliy <z.kon2009@gmail.com>
2016-08-26 01:50:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  */
  
 include 'СhangeOrder.php';
-include 'MakeOrder.php';
+//include 'MakeOrder.php';
 file_put_contents("fb.txt",file_get_contents("php://input"));
 $fb = file_get_contents("fb.txt");
 $fb = json_decode($fb);
@@ -18,7 +18,7 @@ $token = "EAAUZC7GZBxEEoBANtt0BRptegICYxIeE91N0Xt5KQZAHJAmeREQbdl6SZAsNWWplnP2OD
 $fp = json_decode(file_get_contents('user.json'), true);
 
 $rus = new СhangeOrder;
-$MakeCheck = new MakeOrder;
+//$MakeCheck = new MakeOrder;
 
 $dbHost='upperl.mysql.ukraine.com.ua';// чаще всего это так, но иногда требуется прописать ip адрес базы данных
 $dbName='upperl_vadik';// название вашей базы
@@ -220,7 +220,7 @@ switch ($message) {
                 }
             }
             
-           //$data = $MakeCheck->makePizzaType($rows,$id,$keyboardMenu);
+           
            
            $findme   = ',';
            $button = explode($findme, $rows['pizzaType']);
