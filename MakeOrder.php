984434316a23984ffb3d474af1c5d5377706afde
Basilliy <z.kon2009@gmail.com>
2016-08-26 10:27:19 +0300

    Update MakeOrder.php

--- MakeOrder.php
+++ MakeOrder.php
@@ -2,27 +2,10 @@
 
 class MakeOrder{
   
-  function makePizzaType($rows,$id){
-     $findme   = ',';
-           $button = explode($findme, $rows['pizzaType']);
-           
-           for($i = 0; $i < count($button); $i ++){
-            
-            ${$button[$i]}= array(
-            "content_type" => "text",
-            "title" => "$button[$i]",
-            "payload" => "$button[$i]"
-            );
-            $keyboardMenu[$i] = ${$button[$i]};
-            
-            }
-           $data = array(
-           'recipient' => array('id' => "$id" ),
-           'message' => array("text" => "Pizza type",
-           "quick_replies" => json_encode($keyboardMenu)
-           )
-           );
-    return $data;
+  function printHi(){
+    $hi = "russik make order";
+    return $hi;
+    
   }
   
   
