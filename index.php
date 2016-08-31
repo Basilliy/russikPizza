d462d107f998d2bcaa5965840df75974602287a0
Basilliy <z.kon2009@gmail.com>
2016-08-31 12:48:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -280,9 +280,9 @@ switch ($message) {
             }
            }
            
-          // $url = "https://presentpizza.herokuapp.com/Neapolitan.jpg"
+           $url = "https://presentpizza.herokuapp.com/Neapolitan.jpg";
            $attachment = array( "type" => "image",
-            "payload" => array("url" => "https://presentpizza.herokuapp.com/Neapolitan.jpg")
+            "payload" => array("url" => "$url")
             );
            
             $data = array(
